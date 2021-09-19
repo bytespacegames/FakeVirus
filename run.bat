1500f07fd35df1269d0a063d0e20a09d7eb067ce
space <55336451+bytespacegames@users.noreply.github.com>
2021-09-19 12:01:52 -0600

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,157 @@
+@echo off
+title FV
+cmdow.exe "FV" /MOV 4 6
+echo This software is harmless, but could be considered malware, please make sure you have closed any unsaved work beforehand
+echo This will possibly lag or glitch out your computer depending on the specs and may spam windows.
+echo[
+echo Would you like to proceed? (y/n)
+set /p resp=
+if %resp% == n (exit)
+if %resp% == y (goto virus)
+:virus
+cmdow @ /hid
+start errors/Error1.vbs
+TIMEOUT /T 2 >nul
+start errors/Error1.vbs
+TIMEOUT /T 2 >nul
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+start errors/Error2.vbs
+TIMEOUT /T 1 >nul
+start errors/bonzi.vbs
+cmdow "Bonzi Greeting" /MOV 367 400
+start errors/sys.vbs
+cmdow "FREE VBUCK WARN" /MOV 534 845
+echo Bruh
+start errors/bruh.vbs
+cmdow "brbrbrbrbRUH" /MOV 500 560
+start actions/speak.vbs
+start errors/bruh.vbs
+start errors/bruh.vbs
+start actions/speak.vbs
+TIMEOUT /T 1 >nul
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/error.vbs
+start errors/StillUsing.vbs
+TIMEOUT /T 2 >nul
+start actions/mspaintxp.exe
+TIMEOUT /T 2 >nul
+start actions/cmdxp.exe
+start C:/Windows/system32
+TIMEOUT /T 1 >nul
+start C:/Windows
+TIMEOUT /T 1 >nul
+start errors/end1.vbs
+cmdow "goodbye" /MOV 6 4
+TIMEOUT /T 1 >nul
+start errors/end2.vbs
+cmdow "goodbye" /MOV 106 4
+TIMEOUT /T 1 >nul
+start errors/end3.vbs
+cmdow "goodbye" /MOV 206 4
+TIMEOUT /T 1 >nul
+shutdown -s -f
+pause
